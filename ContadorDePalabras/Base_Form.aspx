bc1e08193b3636db61c6a17edad043220a6da992
Victor Pérez <v.perezasuaje@gmail.com>
2020-05-17 14:14:18 +0200

    Word counter, char counter and char without spaces counter added

---
+++ ContadorDePalabras/Base_Form.aspx
@@ -0,0 +1,39 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Base_Form.aspx.cs" Inherits="ContadorDePalabras.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Word Counter</title>
+    <style type="text/css">
+        .area-cuadro-texto {
+            width: 80%;
+            height: 240px;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <h1>Word counter online</h1>
+            <br />
+        </div>
+        <div class="form-group">
+
+            <asp:TextBox ID="TxtBox" runat="server" class="area-cuadro-texto"></asp:TextBox>
+            <br />
+            <br />
+            <asp:Label ID="WordsLbl" runat="server" Text="Word number: "></asp:Label>
+            <br />
+            <br />
+            <asp:Label ID="CharLbl" runat="server" Text="Char number: "></asp:Label>
+            <br />
+            <br />
+            <asp:Label ID="CharLblWS" runat="server" Text="Char number without spaces: "></asp:Label>
+            <br />
+            <br />
+        </div>
+        <asp:Button ID="SubmitBtn" runat="server" OnClick="SubmitBtn_Click" Text="Submit" />
+    </form>
+</body>
+</html>
---
+++ ContadorDePalabras/Base_Form.aspx.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ContadorDePalabras
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void SubmitBtn_Click(object sender, EventArgs e)
+        {
+            int numeroPalabras = 0, restaPalabras = 0;
+            char[] buscadorEspacios = new char[50000];
+            CharLblWS.Visible = false;
+
+            if(TxtBox.Text == String.Empty)
+            {
+                WordsLbl.Text = "Word number: " + numeroPalabras.ToString();
+                CharLbl.Text = "Char number: " + TxtBox.Text.ToCharArray().Length;
+            }
+            else
+            {
+                buscadorEspacios = TxtBox.Text.ToCharArray();
+                CharLblWS.Visible = true;
+                for (int i = 1; i < TxtBox.Text.ToCharArray().Length; i++)
+                {
+                    if (i == 1 && buscadorEspacios[0] == ' ')
+                    {
+                        restaPalabras++;
+                    }
+                    else if (buscadorEspacios[i] == ' ' && buscadorEspacios[i - 1] == ' ')
+                    {
+                        restaPalabras++;
+                    }
+                    else
+                    {
+                        continue;
+                    }
+                }
+                numeroPalabras = TxtBox.Text.ToCharArray().Count(s => s == ' ') + 1 - restaPalabras;
+                WordsLbl.Text = "Word number: " + numeroPalabras.ToString();
+                CharLbl.Text = "Char number: " + TxtBox.Text.ToCharArray().Length;
+                CharLblWS.Text = "Char number without spaces: " + (TxtBox.Text.ToCharArray().Length - TxtBox.Text.ToCharArray().Count(s => s == ' '));
+            }
+            
+        }
+    }
+}
